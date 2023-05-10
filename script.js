de7fb60ecc0d751fb328311e50ba6a88ce81cccb
Mihaita-Daniel Bustea <mihai.bustea@yahoo.com>
2023-05-10 21:02:05 +0300

    Add a function for playing a round

--- script.js
+++ script.js
@@ -16,5 +16,19 @@ function getPlayerChoice() {
             alert("You did not make a valid choice, please try again!");
         }
     }
+}
 
+function playRound(playerSelection, computerSelection) {
+    let winner = undefined;
+    if (playerSelection === computerSelection) {
+        return "It's a TIE, the player chose " + playerSelection + " while the computer chose " + computerSelection + ".";
+    } else if (playerSelection === "rock" && computerSelection === "scrissors" || playerSelection === "scrissors" && computerSelection === "paper" || playerSelection === "paper" && computerSelection === "rock") {
+        winner = "The player";
+        console.log(winner + " won. They chose " + playerSelection + " while the computer chose " + computerSelection + ".");
+        return winner;
+    } else {
+        winner = "The computer";
+        console.log(winner + " won. They chose " + computerSelection + " while the player chose " + playerSelection+ ".");
+        return winner;
+    }
 }
